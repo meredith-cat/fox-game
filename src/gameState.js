33e0841c9d3d29e25e3dde9845f1d40d9deaccb1
Meredith Gallop <meredithgallop@Merediths-MacBook-Pro.local>
2021-11-29 22:27:33 +0000

    css + ui interaction lessons

--- src/gameState.js
+++ src/gameState.js
@@ -1,11 +1,20 @@
+//import { modFox, modScene, togglePoopBag } from "./ui";
+
 const gameState = {
   current: "INIT",
   clock: 1,
   tick() {
     this.clock++;
-    console.log("clock", this.clock);
+
+    if (this.clock === this.wakeTime) {
+      this.wake();
+    }
+
     return this.clock;
+  },
+  handleUserAction(icon) {
+    console.log(icon);
   },
 };
 
-export default gameState;
+module.exports = gameState;
